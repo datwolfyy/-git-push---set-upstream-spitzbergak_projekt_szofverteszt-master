a5f050236fb24cb871f060f89f3a248070231b57
Szabó Noé Bendegúz <12c-szabo@ipari.vein.hu>
2024-04-18 12:10:57 +0200

    modified:   css/index.css 	modified:   index.html 	modified:   js/index.js

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ window.addEventListener("load", () => {
     let kep_divek = document.querySelectorAll("#kepek div");
     kep_divek.forEach(async (div) => {
         let kep = document.createElement("img");
-        let valasz = await fetch("https://api.unsplash.com/photos/random?query=svalbard", {
+        let valasz = await fetch("https://api.unsplash.com/photos/random?query=svalbard&orientation=landscape", {
             "method": "GET",
             "headers": {
                 "Content-Type": "application/json",
