fea2602e9bf23f8cf50b79f1eba150fe452507cc
Szabó Noé Bendegúz <12c-szabo@ipari.vein.hu>
2024-04-18 12:02:20 +0200

    new file:   .gitignore 	modified:   index.html 	modified:   js/index.js

---
+++ js/index.js
@@ -0,0 +1,18 @@
+window.addEventListener("load", () => {
+    let kep_divek = document.querySelectorAll("#kepek div");
+    kep_divek.forEach(async (div) => {
+        let kep = document.createElement("img");
+        let valasz = await fetch("https://api.unsplash.com/photos/random?query=svalbard", {
+            "method": "GET",
+            "headers": {
+                "Content-Type": "application/json",
+                "Accept-Version": "v1",
+                "Authorization": "Client-ID " + config.UNSPLASH_ACCESS_KEY
+            }
+        });
+        let kep_valasz = await valasz.json();
+        console.log(kep_valasz);
+        kep.src = kep_valasz.urls.small;
+        div.appendChild(kep);
+    });
+});
