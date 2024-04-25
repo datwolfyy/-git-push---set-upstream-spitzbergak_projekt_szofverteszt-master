5b71a8b2c0c0ee354d72c37be4520e7261c5166c
Szabó Noé Bendegúz <12c-szabo@ipari.vein.hu>
2024-04-25 11:48:14 +0200

    modified:   js/index.js

--- js/index.js
+++ js/index.js
@@ -20,36 +20,34 @@ function stickyNavbar() {
 
 //Select
 
-document.addEventListener('DOMContentLoaded', function() {
-  const optionSelector = document.getElementById('optionSelector');
-  const allatContent = document.getElementById('allatContent');
-  optionSelector.addEventListener('change', function() {
-      const selectedOption = optionSelector.value;
-      let selectedContent = '';
-
-      switch(selectedOption) {
-          case 'jegesmedve':
-              selectedContent = "A Spitzbergák ikonikus lakója, a jegesmedve, a sarkvidék egyik legnagyobb ragadozója. Hatalmas testének és vastag bundájának köszönhetően képes túlélni a hideg éghajlatot. A jegesmedvék fő élelmiszerforrása a fóka, amelyet a jégmezőkön zsákmányolnak. Az éghajlatváltozás következtében a jegesmedvék számára egyre nehezebbé válik a túlélés, mivel elveszítik a fókákra vadászásra alkalmas jégmezőket.";
-              break;
-          case 'sarkiroka':
-              selectedContent = "A Spitzbergák másik jellegzetes állata a sarki róka, amely a sarkvidéki területek adaptív mesterségeként él. A sarki rókák fehér bundájukkal és kicsi fülükkel alkalmazkodtak a hideg éghajlathoz. Kiváló ragadozók, és a jegesmedvék után a második legnagyobb számban találhatók meg a Spitzbergákon. Élelmüket főként kisemlősökből és madarakból szerzik, de nem vetik meg a halakat sem.";
-              break;
-          case 'renszarvas':
-              selectedContent = "A rénszarvasok a Spitzbergák fő emlős lakói, és jellegzetes látványt nyújtanak a tundrákon és a hegyekben. Ezek a páratlanul széles agancsú emlősök a tél kemény éghajlatához alkalmazkodtak, vastag szőrzettel és kerek lábakkal rendelkeznek. A rénszarvasok nagy csoportokban élnek, és főként a tundra növényeivel táplálkoznak, beleértve a moha és zuzmót.";
-              break;
-          case 'gyongybagoly':
-              selectedContent = "A Spitzbergák madárvilágának egyik jellegzetes képviselője a gyöngybagoly, amely szintén sarkvidéki alkalmazkodásokkal rendelkezik. A gyöngybagoly rendkívül nagy faj, súlyos testtömegével és vastag szőrzetével, amely lehetővé teszi számára, hogy a hideg éghajlatban is túléljen. A Spitzbergákon a gyöngybagoly fő zsákmányai közé tartoznak a rágcsálók és a madarak.";
-              break;
-          default:
-              selectedContent = "Nincs kiválasztott tartalom.";
-      }
-
-      allatContent.innerHTML = `<div class="selectedContent">${selectedContent}</div>`;
-  });
-});
 
 
 window.addEventListener("load", () => {
+    const optionSelector = document.getElementById('optionSelector');
+    const allatContent = document.getElementById('allatContent');
+    optionSelector.addEventListener('change', function() {
+        const selectedOption = optionSelector.value;
+        let selectedContent = '';
+  
+        switch(selectedOption) {
+            case 'jegesmedve':
+                selectedContent = "A Spitzbergák ikonikus lakója, a jegesmedve, a sarkvidék egyik legnagyobb ragadozója. Hatalmas testének és vastag bundájának köszönhetően képes túlélni a hideg éghajlatot. A jegesmedvék fő élelmiszerforrása a fóka, amelyet a jégmezőkön zsákmányolnak. Az éghajlatváltozás következtében a jegesmedvék számára egyre nehezebbé válik a túlélés, mivel elveszítik a fókákra vadászásra alkalmas jégmezőket.";
+                break;
+            case 'sarkiroka':
+                selectedContent = "A Spitzbergák másik jellegzetes állata a sarki róka, amely a sarkvidéki területek adaptív mesterségeként él. A sarki rókák fehér bundájukkal és kicsi fülükkel alkalmazkodtak a hideg éghajlathoz. Kiváló ragadozók, és a jegesmedvék után a második legnagyobb számban találhatók meg a Spitzbergákon. Élelmüket főként kisemlősökből és madarakból szerzik, de nem vetik meg a halakat sem.";
+                break;
+            case 'renszarvas':
+                selectedContent = "A rénszarvasok a Spitzbergák fő emlős lakói, és jellegzetes látványt nyújtanak a tundrákon és a hegyekben. Ezek a páratlanul széles agancsú emlősök a tél kemény éghajlatához alkalmazkodtak, vastag szőrzettel és kerek lábakkal rendelkeznek. A rénszarvasok nagy csoportokban élnek, és főként a tundra növényeivel táplálkoznak, beleértve a moha és zuzmót.";
+                break;
+            case 'gyongybagoly':
+                selectedContent = "A Spitzbergák madárvilágának egyik jellegzetes képviselője a gyöngybagoly, amely szintén sarkvidéki alkalmazkodásokkal rendelkezik. A gyöngybagoly rendkívül nagy faj, súlyos testtömegével és vastag szőrzetével, amely lehetővé teszi számára, hogy a hideg éghajlatban is túléljen. A Spitzbergákon a gyöngybagoly fő zsákmányai közé tartoznak a rágcsálók és a madarak.";
+                break;
+            default:
+                selectedContent = "Nincs kiválasztott tartalom.";
+        }
+  
+        allatContent.innerHTML = `<div class="selectedContent">${selectedContent}</div>`;
+    });  
     let kep_divek = document.querySelectorAll("#kepek div");
     kep_divek.forEach(async (div) => {
         let kep = document.createElement("img");
@@ -66,4 +64,5 @@ window.addEventListener("load", () => {
         kep.src = kep_valasz.urls.small;
         div.appendChild(kep);
     });
+    
 });
